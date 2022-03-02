226824d09b3c70f24ae2d6c5b0afc9f67d9f414c
Posey <adeposi60@gmail.com>
2022-03-02 23:12:29 +0100

    Added game cards

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,10 +1,30 @@
 import React, { useContext } from 'react';
+import { FaAndroid, FaApple, FaAppStoreIos, FaLinux, FaPlaystation, FaWindows, FaXbox } from 'react-icons/fa';
 import { GameContext } from '../Context';
+
+function parsePlatform(platform){
+   
+    if (platform === "PC"){
+        return <FaWindows/>
+    }else if(platform === "PlayStation"){
+        return <FaPlaystation/>
+    }else if(platform === "Apple Macintosh"){
+        return <FaApple/>
+    }else if(platform === "Linux"){
+        return <FaLinux/>
+    }else if(platform === "Android"){
+        return <FaAndroid/>
+    }else if (platform === "iOS"){
+        return <FaAppStoreIos/>
+    }else if (platform === "Xbox"){
+        return <FaXbox/>
+    }
+}
 
 function Explore(props) {
     const {test} = useContext(GameContext)
     return (
-        <div className="col-span-10 md:col-span-7 rounded-3xl text-typography-white">
+        <div className="col-span-10 md:col-span-7 bg-black-v2 rounded-3xl p-4 text-typography-white">
             <div className="grid grid-cols-3 gap-4">
                 
                     {test.results.map(result=>{
@@ -17,11 +37,12 @@ function Explore(props) {
                         <div className="text-center">
                             <h1 className="text-lg">{name}</h1>
                             
-                        
-                            {parent_platforms.map(platform=><div className="text-typography-white">
-                                <p>{platform.platform.name}</p>
-                                <img/>
+                            <div className="flex gap-2 text-lg">
+                            {parent_platforms.map(platform=><div className="text-brand-purple">
+                                <i>{parsePlatform(platform.platform.name)}</i>
                                 </div>)}
+                            </div>
+                           
                         </div>
                     </div>
                         )
