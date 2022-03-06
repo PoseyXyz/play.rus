a0eae38ecc31ab155617c8741012b8c84214d379
Posey <adeposi60@gmail.com>
2022-03-06 06:07:44 +0100

    Completed sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,12 +9,13 @@ import { GiCrossedPistols } from 'react-icons/gi'
 function Sidebar({ sideBarOpen, setSideBarOpen }) {
 
     return (
-        <div className={`${sideBarOpen ? 'w-full' : 'w-0'} fixed h-screen bg-black z-20 top-0 right-0 duration-700 overflow-hidden`}>
-            <button className="text-white text-lg" onClick={() => setSideBarOpen(false)}><i><FaTimesCircle /></i></button>
+        <div className={`${sideBarOpen ? 'w-full' : 'w-0'} fixed overflow-y-scroll bg-black h-screen z-20 top-0 right-0 duration-700 py-3 overflow-x-hidden flex flex-col`}>
 
-            <div className="flex flex-col text-white px-6 gap-2">
-                <div className="bg-brand-purple rounded-full p-2 px-4 self-center">
-                    <p className="text-white text-lg font-bold">A</p>
+            <button className="text-white right-4 text-4xl top-4 self-end mr-4" onClick={() => setSideBarOpen(false)}><i><FaTimesCircle /></i></button>
+
+            <div className="flex flex-col text-white px-6 gap-2 mt-4">
+                <div className="bg-brand-purple rounded-full p-3 px-5 self-center">
+                    <p className="text-white text-xl font-bold">A</p>
                 </div>
                 <div className="flex flex-col gap-4">
                     <div className="flex justify-between gap-4">
@@ -22,33 +23,31 @@ function Sidebar({ sideBarOpen, setSideBarOpen }) {
                         <p className="font-semibold">850 XP</p>
                     </div>
                     <div className="">
-                        <span className="w-full h-2 rounded-lg bg-typography-grey block"><span className="rounded-lg w-3/5 h-2 block bg-brand-purple"></span></span>
-
+                        <span className="w-full h-3 rounded-lg bg-typography-grey block"><span className="rounded-lg w-3/5 h-3 block bg-brand-purple"></span></span>
                     </div>
                 </div>
 
-                <div className="my-4 flex flex-col gap-6">
-                    <div className="menubar-link"><i><FaHome /></i><h1>Explore</h1></div>
+                <div className="my-8 flex flex-col gap-8">
+                    <div className="sidebar-link"><i><FaHome /></i><h1>Explore</h1></div>
 
-                    <div className="menubar-link text-white"><i><FaHistory /></i><h1>Recents</h1></div>
+                    <div className="sidebar-link text-white"><i><FaHistory /></i><h1>Recents</h1></div>
 
-                    <hr className="mx-8 border-black-v3"></hr>
 
                     <div className="flex flex-col gap-8">
-                        <div className="menubar-link"><i><ImBooks /></i><h1>My Library</h1></div>
+                        <div className="sidebar-link"><i><ImBooks /></i><h1>My Library</h1></div>
 
                     </div>
 
-                    <hr className="mx-8 border-black-v3"></hr>
 
-                    <div className="menubar-link"><i><GiCrossedPistols /></i><h1>Genres</h1></div>
-                    <div className="menubar-link"><i><FaCode /></i><h1>Developers</h1></div>
-                    <div className="menubar-link"><i><CgGames /></i><h1>Platforms</h1></div>
-                    <div className="menubar-link"><i><FaDownload /></i><h1>Stores</h1></div>
 
-                    <hr className="mx-8 border-black-v3"></hr>
+                    <div className="sidebar-link"><i><GiCrossedPistols /></i><h1>Genres</h1></div>
+                    <div className="sidebar-link"><i><FaCode /></i><h1>Developers</h1></div>
+                    <div className="sidebar-link"><i><CgGames /></i><h1>Platforms</h1></div>
+                    <div className="sidebar-link"><i><FaDownload /></i><h1>Stores</h1></div>
 
-                    <div className="menubar-link"><i><FaSignOutAlt /></i><h1>Logout</h1></div>
+
+
+                    <div className="sidebar-link"><i><FaSignOutAlt /></i><h1>Logout</h1></div>
                 </div>
             </div>
 
