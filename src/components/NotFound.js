bc7311dbbbbff85b846e97c152c835407c1826e2
Posey <adeposi60@gmail.com>
2022-03-09 01:10:31 +0100

    Added html to 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFound(props) {
     return (
-        <div>
-            Not found
+        <div className="h-screen w-screen relative">
+            <img src={require('../images/notfound/404.png')} className="absolute z-10 h-screen w-screen"/>
+            <div className="relative z-20 overlay w-screen h-screen flex flex-col justify-center items-center gap-6 text-white">
+                <h1 className="text-6xl font-bold">404</h1>
+                <p className="text-xl font-medium text-center max-w-xs md:max-w-xl">Sorry, can't seem to find the page you were looking for</p>
+                <Link to='/'>
+                <button className="bg-white text-black-v1 px-8 py-3 text-xl font-semibold hover:bg-brand-purple">Main Page</button></Link>
+            </div>
         </div>
     );
 }
