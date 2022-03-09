416c5587783b86b9f041999721dc3ca9efb5986e
Posey <adeposi60@gmail.com>
2022-03-09 01:25:36 +0100

    Completed 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 function NotFound(props) {
     return (
         <div className="h-screen w-screen relative">
-            <img src={require('../images/notfound/404.png')} className="absolute z-10 h-screen w-screen"/>
+            <img src={require('../images/notfound/404.png')} className="absolute z-10 h-screen w-screen object-cover"/>
             <div className="relative z-20 overlay w-screen h-screen flex flex-col justify-center items-center gap-6 text-white">
                 <h1 className="text-6xl font-bold">404</h1>
                 <p className="text-xl font-medium text-center max-w-xs md:max-w-xl">Sorry, can't seem to find the page you were looking for</p>
                 <Link to='/'>
-                <button className="bg-white text-black-v1 px-8 py-3 text-xl font-semibold hover:bg-brand-purple">Main Page</button></Link>
+                <button className="bg-white text-black-v1 px-8 py-3 text-xl font-semibold hover:bg-typography-grey delay-75 duration-300 hover:text-white">Main Page</button></Link>
             </div>
         </div>
     );
