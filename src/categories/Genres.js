cffb4571b7f2d17d4432fa4fc3f76433e3005fbf
Posey <adeposi60@gmail.com>
2022-03-13 22:16:50 +0100

    Added markup to genres component

--- src/categories/Genres.js
+++ src/categories/Genres.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect, useContext, setState } from 'react';
+import { GameContext } from '../Context';
 
 function Genres(props) {
+    const {getGenres, genres} = useContext(GameContext)
+    useEffect(()=>{
+        getGenres()
+    }, [genres])
     return (
         <div>
-            
+           {genres.map(genre=><h1>{genre.name}</h1>)} 
         </div>
     );
 }
