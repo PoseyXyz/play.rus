565f21ef8df6227b9750b6e7cae919530447200b
Posey <adeposi60@gmail.com>
2022-03-15 23:07:17 +0100

    Slight cleanup and grid styling for cards

--- src/categories/GamesList.js
+++ src/categories/GamesList.js
@@ -1,14 +1,15 @@
-import React, {useContext} from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
 import Card from '../components/Card';
 import { GameContext } from '../Context';
 
 function GamesList(props) {
-    const {gamesList, parsePlatform, parseRatingColour} = useContext(GameContext)
+    const { gamesList, parsePlatform, parseRatingColour } = useContext(GameContext)
     return (
         <div className='outlet-layout'>
-        
-            {gamesList&&gamesList.map(result=><Card result={result} parsePlatform={parsePlatform} parseRatingColour={parseRatingColour}/>)}
+            <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6'>
+                {gamesList && gamesList.map(result => <Card result={result} parsePlatform={parsePlatform} parseRatingColour={parseRatingColour} />)}
+
+            </div>
         </div>
     );
 }
