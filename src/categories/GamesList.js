c26b53f54d0a0803316e7c94c2d167c3f672706b
Posey <adeposi60@gmail.com>
2022-03-15 22:57:18 +0100

    Fixed link parsing error

--- src/categories/GamesList.js
+++ src/categories/GamesList.js
@@ -7,6 +7,7 @@ function GamesList(props) {
     const {gamesList, parsePlatform, parseRatingColour} = useContext(GameContext)
     return (
         <div className='outlet-layout'>
+        
             {gamesList&&gamesList.map(result=><Card result={result} parsePlatform={parsePlatform} parseRatingColour={parseRatingColour}/>)}
         </div>
     );
--- src/categories/MiniCard.js
+++ src/categories/MiniCard.js
@@ -6,12 +6,21 @@ import { useNavigate } from 'react-router-dom';
 function MiniCard({ result, fetchGamesList, type }) {
     const { id, name, games_count, image_background, slug } = result
     let navigate = useNavigate()
+    const parseSlug=()=>{
+        if (type==='platforms'){
+            return id
+        }else if(type==='stores'){
+            return id
+        }else{
+            return slug
+        }
+    }
     return (
         <div className='relative mini-card'>
             <div className='absolute w-full h-full z-10 flex flex-col items-center justify-center overlay'>
                 <div className='flex flex-col gap-2 text-center'>
-                    <h1 className='text-xl font-sans font-bold'>{name} {id} {slug}</h1>
-                    <button onClick={()=>{fetchGamesList(type, slug); navigate(`/games/${slug}`)}} className='py-2.5 px-4 btn-overlay self-center rounded-lg hover:bg-white hover:text-black duration-150 delay-75 font-semibold'>View Games</button>
+                    <h1 className='text-xl font-sans font-bold'>{name} {id} {slug} {parseSlug()}</h1>
+                    <button onClick={()=>{fetchGamesList(type, parseSlug()); navigate(`/games/${parseSlug}`)}} className='py-2.5 px-4 btn-overlay self-center rounded-lg hover:bg-white hover:text-black duration-150 delay-75 font-semibold'>View Games</button>
                 </div>
                 <div className='flex justify-between border-b absolute w-11/12 bottom-4 pb-2 text-sm'>
                 <p className='font-semibold'>Game Count:</p>
