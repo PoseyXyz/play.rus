b6b576a43585bf10b5612dce51064b33aa1320e8
Posey <adeposi60@gmail.com>
2022-03-17 07:04:59 +0100

    Setup fetch to retrieve full data on a single game for the details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
 
 function Details(props) {
+    const [result, setResult] = useState()
+    let {slug} = useParams()
+    const fetchDetails=async()=>{
+        const data = await fetch(`https://api.rawg.io/api/games/${slug}?key=9df1bae5b88947458cc8431730fbfd9f`)
+        const res = await data.json()
+        setResult(res)
+    }
+   useEffect(()=>{
+    fetchDetails()
+   }, [])
     return (
-        <div>
-            
+        <div className='outlet-layout'>
+            <p>{result && result.name }</p>
+            <button onClick={fetchDetails}>Click me</button>
         </div>
     );
 }
