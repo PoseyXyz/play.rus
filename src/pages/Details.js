2f3a05176644b66002173bb4e3344c987d95f9e5
Posey <adeposi60@gmail.com>
2022-03-18 00:52:13 +0100

    Deleted details component

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 
 function Details(props) {
-    const [result, setResult] = useState()
+    const [result, setResult] = useState({})
     let {slug} = useParams()
     const fetchDetails=async()=>{
         const data = await fetch(`https://api.rawg.io/api/games/${slug}?key=9df1bae5b88947458cc8431730fbfd9f`)
@@ -12,12 +12,38 @@ function Details(props) {
    useEffect(()=>{
     fetchDetails()
    }, [])
+   const {name, background_image_additional} = result 
+
+   
     return (
         <div className='outlet-layout'>
-            <p>{result && result.name }</p>
+          <LazyLoadImage
+                    className="rounded-t-xl h-full w-screen object-cover"
+                    height={'100%'}
+                    effect="blur"
+                    src={background_image_additional} />
+            <img src={result&&result.background_image}/>
+            <img src={result&&result.background_image_additional}/>
+            <img src={result&&result.reddit_logo}/>
+            <p>{name}</p>
+           
+            {result && result.platforms && result.platforms.map(platform=><p>{platform.requirements.recommended}</p>)}
             <button onClick={fetchDetails}>Click me</button>
         </div>
     );
 }
 
 export default Details;
+
+// name
+// metacritic
+// released
+// website
+// rating
+// playtime
+// platforms
+// reddit description
+// esrb rating
+// description(genres)
+// similar games
+// requirements
