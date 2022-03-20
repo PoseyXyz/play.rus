137bb95305fa4acbadce18045da20387be1a16c4
Posey <adeposi60@gmail.com>
2022-03-20 04:37:02 +0100

    Created select chat buttons

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {FaSearch} from 'react-icons/fa'
+import { FaSearch, FaUserFriends } from 'react-icons/fa'
 import image1 from '../images/friends/one.png'
 import image2 from '../images/friends/two.png'
 import image3 from '../images/friends/three.png'
@@ -125,10 +125,36 @@ function ChatPage(props) {
         <div className='outlet-layout'>
             <div className='grid grid-cols-3 gap-6'>
                 {/* friends box */}
-                <div className='col-span-1 bg-black-v3 h-screen rounded-xl lg:p-8'>
-                    <div className='relative flex'>
-                        <i className='absolute self-center left-4'><FaSearch/></i>
-                        <input placeholder='Search' className='rounded-3xl post-box w-full p-3 px-10 outline-none text-typography-grey text-sm tracking-wide focus:text-white border border-transparent delay-75 duration-150 focus:border-brand-purple'/>
+                <div className='col-span-1 bg-black-v3 h-screen rounded-xl lg:p-6 flex flex-col gap-6'>
+
+                    {/* search bar and friend icon */}
+                    <div className='flex gap-4 justify-between'>
+                        <div className='relative flex w-4/5'>
+                            <i className='absolute self-center left-4 text-sm'><FaSearch /></i>
+                            <input placeholder='Search' className='rounded-3xl post-box w-full p-2.5 px-10 outline-none text-typography-grey text-sm tracking-wide focus:text-white border border-transparent delay-75 duration-150 focus:border-brand-purple' />
+                        </div>
+                        <div className='bg-black-v3 rounded-full post-box flex items-center px-4 py-2'>
+                        <i className=''>
+                            <FaUserFriends/>
+                        </i>
+                        </div>
+                    </div>
+
+                    {/* friends list */}
+                    <div className='flex flex-col gap-4'>
+                        {chats.map(chat=>{
+                            const {id, name, imageUrl, chatHistory} = chat
+                            return(
+                                <div className='flex flex-col lg:flex-row gap-4 post-box p-4 rounded-xl' key={id}>
+                                    <img src={imageUrl} className='rounded-full w-10 h-10' />
+                                    <div className='flex flex-col'>
+                                        <span className='flex gap-2 justify-between items-center'><span className='text-sm'>{name}</span><span className='flex text-xs text-typography-grey font-medium'>22:30</span></span>
+                                        <p className='text-typography-grey text-sm tracking-wide leading-6'>{chatHistory[chatHistory.length-1].message}</p>
+                                    </div>
+
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
 
