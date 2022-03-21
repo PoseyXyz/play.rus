fb330084636c59007e9d8403756cedabd8049410
Posey <adeposi60@gmail.com>
2022-03-21 03:37:29 +0100

    Set fixed height for outlet

--- src/components/FriendsBar.js
+++ src/components/FriendsBar.js
@@ -5,13 +5,13 @@ function FriendsBar(props) {
         <aside className="bg-black-v1 hidden md:block col-span-1 sticky top-24 friendsbar box-border rounded-r-lg">
             <div className="flex flex-col gap-10 py-8">
                 <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/one.png')} className="avatar border-brand-green" /></div>
-                <div className="friend-icon"><div className="status-dot bg-red-500"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/two.png')} className="border-brand-purple avatar" /></div>
+                <div className="friend-icon"><div className="status-dot bg-red-500"></div><div className="xp"><p className="text-white">8</p></div><img src={require('../images/friends/two.png')} className="border-brand-purple avatar" /></div>
                 <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/three.png')} className="border-red-500 avatar" /></div>
-                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/four.jpg')} className="border-blue-500 avatar" /></div>
+                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">12</p></div><img src={require('../images/friends/four.jpg')} className="border-blue-500 avatar" /></div>
                 <div className="friend-icon"><div className="status-dot bg-red-500"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/five.png')} className="border-orange-500 avatar" /></div>
-                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/six.png')} className="border-white avatar" /></div>
-                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/seven.png')} className="border-white avatar" /></div>
-                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">10</p></div><img src={require('../images/friends/eight.jpg')} className="border-yellow-500 avatar" /></div>
+                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">11</p></div><img src={require('../images/friends/six.png')} className="border-white avatar" /></div>
+                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">7</p></div><img src={require('../images/friends/seven.png')} className="border-white avatar" /></div>
+                <div className="friend-icon"><div className="status-dot bg-brand-green"></div><div className="xp"><p className="text-white">6</p></div><img src={require('../images/friends/eight.jpg')} className="border-yellow-500 avatar" /></div>
             
             </div>
         </aside>
