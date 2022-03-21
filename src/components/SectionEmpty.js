5e0ffb40ada7c1fc31dac3005c8a38ef101eae24
Posey <adeposi60@gmail.com>
2022-03-21 19:31:25 +0100

    Added markup to section empty component and imported component into Library page

--- src/components/SectionEmpty.js
+++ src/components/SectionEmpty.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import svg from '../images/notfound/emoji.svg'
 
 function SectionEmpty(props) {
     return (
-        <div>
-            
-        </div>
+        <div className='flex justify-center flex-col my-4 items-center lg:text-lg'>
+                    <img src={svg} className="h-1/3 w-1/3" alt="Not found in library emoji" />
+                    <p>Seems like there are no games here</p>
+                </div>
     );
 }
 
--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from 'react';
 import Card from '../components/Card';
+import SectionEmpty from '../components/SectionEmpty';
 import { GameContext } from '../Context';
-import svg from '../images/notfound/emoji.svg'
+
 
 
 
@@ -15,10 +16,7 @@ function Library() {
                 {Object.keys(librarySections).map(key => <button onClick={() => { setCurrentKey(key) }} className={`${currentKey === key ? 'text-white border-b-2 border-white' : 'border-b-2 border-transparent text-typography-grey'} text-lg py-2 capitalize hover:text-white delay-75 duration-200`}>{key}</button>)}
             </div>
             {
-                librarySections[currentKey].length === 0 ? <div className='flex justify-center flex-col my-4 items-center lg:text-lg'>
-                    <img src={svg} className="h-1/3 w-1/3" alt="Not found in library emoji" />
-                    <p>Seems like there are no games here</p>
-                </div> : <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6'>
+                librarySections[currentKey].length === 0 ? <SectionEmpty/> : <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6'>
                     {librarySections[currentKey].map((result) => <Card key={result.id} result={result} parsePlatform={parsePlatform} parseRatingColour={parseRatingColour} />)}
                 </div>
             }
