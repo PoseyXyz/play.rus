30a47d0365471decb5442e293e2b7ecdec651b0d
Posey <adeposi60@gmail.com>
2022-05-16 04:59:29 +0100

    Second attempt at fixing card sizing issue

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,10 +18,10 @@ function Card({ result, parsePlatformIcons, parseRatingColour, scrollPosition })
     return (
         <div className="card flex flex-col bg-black-v3 rounded-xl relative">
 
-            <div className="bg-red-500 grow flex-1">
+            <div className="h-[250px]">
                 <LazyLoadImage
                     className="rounded-t-xl h-full w-screen object-cover"
-                    height={'100%'}
+                    height={'250px'}
                     width={'100%'}
                     alt='gameCard_image'
                     effect="blur"
@@ -30,10 +30,10 @@ function Card({ result, parsePlatformIcons, parseRatingColour, scrollPosition })
                 
                 {/* <img src={background_image} /> */}
             </div>
-            <div className="flex flex-col gap-5 px-6 py-4 grow flex-1">
+            <div className="flex flex-col gap-5 px-6 py-4">
                 <div className="flex flex-col gap-4">
                     <div className="flex justify-between items-center">
-                        <Link to={`/details/${slug}`} onClick={() => parseRecents(result)} className="text-lg hover:text-typography-grey duration-100">{name}</Link>
+                        <Link to={`/details/${slug}`} onClick={() => parseRecents(result)} className="text-base 2xl:text-lg hover:text-typography-grey duration-100">{name}</Link>
                         <div className={`${parseRatingColour(rating)} border-2 py-1 px-2 rounded-md text-sm`}><p className="font-semibold">{rating}</p></div>
                     </div>
 
